1aa9e48f7d8ea0975fd196a5ef18e4696c733538
aidai <izumisoft1@mail.ru>
2022-10-08 21:14:56 +0300

    Initial commit

---
+++ src/Robot.java
@@ -0,0 +1,13 @@
+public class Robot {
+    String name;
+    int age;
+    String color;
+    double weight;
+
+    public void move(){
+        System.out.println("Robot can move");
+    }
+    public void speak(){
+        System.out.println("Robot can speak");
+    }
+}
